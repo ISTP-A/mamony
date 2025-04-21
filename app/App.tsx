6dc3ea3c317faf568be48e2b31f247e54c85f9e6
ISTP-A <huhuhunuhuhn@gmail.com>
2025-04-22 06:27:25 +0900

    fix: delete sub repo

---
+++ app/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+import './global.css';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text className=''>ㅎㅇㅎㅇ</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
